0be8ebf4bdfbd13f1de984132c7860e99b53f4ca
huemin <100716027+huemin-art@users.noreply.github.com>
2023-03-02 00:15:20 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,38 +1,36 @@
 const folderPath = "mm/samples/"; // Replace with the path to your images folder
 const bgFolderPath = "mm/detail/";
 const imgExtensions = ["jpg", "jpeg", "png", "gif"];
-let predefinedImageNames = ["sample1.png", "sample2.png", "sample3.png", "sample4.png", "sample5.png", "sample6.png", "sample7.png", "sample8.png", "sample9.png"];
+const predefinedImageNames = ["sample1.png", "sample2.png", "sample3.png", "sample4.png", "sample5.png", "sample6.png", "sample7.png", "sample8.png", "sample9.png"];
 
 let images = [];
 let currentImageIndices = {};
+let imageIndices = [];
 let imageFilenames = {};
 
 function getRandomImage(img) {
   const imgClass = img.getAttribute("class");
-  const newImageIndex = getRandomIndex(imageFilenames[imgClass].length);
+  const newImageIndex = imageIndices.pop();
   currentImageIndices[imgClass] = newImageIndex;
   img.src = folderPath + imageFilenames[imgClass][currentImageIndices[imgClass]];
-  imageFilenames[imgClass].splice(newImageIndex, 1);
-}
-
-function getRandomIndex(length) {
-  return Math.floor(Math.random() * length);
 }
 
 const bgImageNames = ["detail1.png", "detail2.png", "detail3.png"];
 
 function setRandomBackgroundImage() {
-  const randomIndex = getRandomIndex(bgImageNames.length);
+  const randomIndex = Math.floor(Math.random() * bgImageNames.length);
   const bgImagePath = bgFolderPath + bgImageNames[randomIndex];
   document.body.style.backgroundImage = `url(${bgImagePath})`;
 }
 
 document.addEventListener("DOMContentLoaded", function() {
   images = Array.from(document.querySelectorAll(".sample"));
+  const numImages = images.length;
+  imageIndices = shuffle([...Array(numImages).keys()]); // create an array of indices and shuffle it
   images.forEach((img) => {
     const imgClass = img.getAttribute("class");
     currentImageIndices[imgClass] = -1;
-    imageFilenames[imgClass] = predefinedImageNames.slice(); // make a copy of the predefinedImageNames array
+    imageFilenames[imgClass] = shuffle(predefinedImageNames.slice()); // shuffle the filenames for each image
     getRandomImage(img);
   });
 
@@ -44,4 +42,20 @@ $(document).ready(function() {
     $('.main').toggleClass('active');
     $('.nav-btn').toggleClass('active');
   });
-})
+});
+
+function shuffle(array) {
+  // Fisher-Yates shuffle algorithm
+  let currentIndex = array.length;
+  let temporaryValue, randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
