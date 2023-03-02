6bffb7377d970857235c026bfb923028506b200e
huemin <100716027+huemin-art@users.noreply.github.com>
2023-03-01 23:58:19 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const folderPath = "mm/samples/"; // Replace with the path to your images folder
 const bgFolderPath = "mm/detail/";
 const imgExtensions = ["jpg", "jpeg", "png", "gif"];
-const predefinedImageNames = ["sample1.png", "sample2.png", "sample3.png", "sample4.png", "sample5.png", "sample6.png", "sample7.png", "sample8.png", "sample9.png"];
+let predefinedImageNames = ["sample1.png", "sample2.png", "sample3.png", "sample4.png", "sample5.png", "sample6.png", "sample7.png", "sample8.png", "sample9.png"];
 
 let images = [];
 let currentImageIndices = {};
@@ -9,26 +9,20 @@ let imageFilenames = {};
 
 function getRandomImage(img) {
   const imgClass = img.getAttribute("class");
-  const newImageIndex = getRandomIndexExcludingCurrent(imageFilenames[imgClass], currentImageIndices[imgClass]);
+  const newImageIndex = getRandomIndex(imageFilenames[imgClass].length);
   currentImageIndices[imgClass] = newImageIndex;
   img.src = folderPath + imageFilenames[imgClass][currentImageIndices[imgClass]];
+  imageFilenames[imgClass].splice(newImageIndex, 1);
 }
 
-function getRandomIndexExcludingCurrent(arr, currentIndex) {
-  if (arr.length <= 1) {
-    return 0;
-  }
-  let randomIndex = Math.floor(Math.random() * arr.length);
-  while (randomIndex === currentIndex) {
-    randomIndex = Math.floor(Math.random() * arr.length);
-  }
-  return randomIndex;
+function getRandomIndex(length) {
+  return Math.floor(Math.random() * length);
 }
 
 const bgImageNames = ["detail1.png", "detail2.png", "detail3.png"];
 
 function setRandomBackgroundImage() {
-  const randomIndex = Math.floor(Math.random() * bgImageNames.length);
+  const randomIndex = getRandomIndex(bgImageNames.length);
   const bgImagePath = bgFolderPath + bgImageNames[randomIndex];
   document.body.style.backgroundImage = `url(${bgImagePath})`;
 }
@@ -38,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function() {
   images.forEach((img) => {
     const imgClass = img.getAttribute("class");
     currentImageIndices[imgClass] = -1;
-    imageFilenames[imgClass] = predefinedImageNames;
+    imageFilenames[imgClass] = predefinedImageNames.slice(); // make a copy of the predefinedImageNames array
     getRandomImage(img);
   });
 
@@ -51,4 +45,3 @@ $(document).ready(function() {
     $('.nav-btn').toggleClass('active');
   });
 })
-
